a835957873200a81f7cb4f1c1c99018bf3af8311
Malpp <cpt.ortan@gmail.com>
2016-10-28 13:55:54 -0400

    Added Tools class

--- src/progTP3/progTP3/Program.cs
+++ src/progTP3/progTP3/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace progTP3
 {
+
+	class Tool
+	{
+
+
+		public static void NiceAssFunction()
+		{
+
+			Console.WriteLine("WALDOINFUSEIBNUGIESANGUV");
+
+		}
+
+	}
+
     class Program
     {
         static void Main(string[] args)
         {
+
+			Console.WriteLine("DU CODE xD");
+
+			Tool.NiceAssFunction();
+
         }
     }
 }
